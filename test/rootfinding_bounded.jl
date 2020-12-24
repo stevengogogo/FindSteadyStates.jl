5f3fdf11c4a9c6b20f651260b4c15a0a5a4241a2
stevengogogo <stevengogogo4321@gmail.com>
2020-12-24 15:25:42 +0800

    Revise rootfinding test

--- test/rootfinding_bounded.jl
+++ test/rootfinding_bounded.jl
@@ -10,15 +10,13 @@ Name tuple is allowed
 
 include("testutils.jl") 
 
-de = DEsteady(func=bistable_ode!, p=p_, u0= u_1, method=SSRootfind(nlsolve = (f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0,autodiff=:forward,method=:newton,iterations=Int(1e6),ftol=abstol);res.zero) ))
+de = DEsteady(func=bistable_ode!, p=Vector(p_), u0= Vector(u_1), method=SSRootfind(nlsolve = (f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0,autodiff=:forward,method=:newton,iterations=Int(1e6),ftol=abstol);res.zero) ))
 
 sol = solve(de)
-
 sol.retcode == :Success
-
 
 #=
 With boundary
 =#
-de_b = DEsteady(de, method=method=SSRootfind(nlsolve = (f,u0,abstol) -> (res=NLsolve.mcpsolve(f,u0, zeros(length(u0)), fill(Inf, length(u0)) ,autodiff=:forward,method=:newton,iterations=Int(1e6),ftol=abstol);res.zero) )
+de_b = DEsteady(de, method=method=SSRootfind(nlsolve = (f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0, zeros(length(u0)), fill(Inf, length(u0)) ,autodiff=:forward,method=:newton,iterations=Int(1e6),ftol=abstol);res.zero) )
 )
